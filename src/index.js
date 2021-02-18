ea07ec2ff061da0dcbf2e240a62a04f29deb3bec
Marina <78230561+Mor-11@users.noreply.github.com>
2021-02-18 15:30:21 +0300

    feat: complete test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str; // капируем строку 
+  let string = bracketsConfig.map((item) => item.join("")); // в массиве делаем из массива строки 
+  for (let i = 0; i < str.length; i++) { 
+    for (let i = 0; i < string.length; i++) {
+      newStr = newStr.replace(string[i], ""); // удаляем скобки парные 
+    }
+  }
+  if (newStr === "") return true; 
+  else return false;
 }
